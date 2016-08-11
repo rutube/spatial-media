2e35272125b5a0cd4590accf279b0c211156f384
tumb1er <zimbler@gmail.com>
2016-08-11 15:44:30 +0300

    Add script bin/spatial_media to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+try:
+    copy("bin/spatial_media.py", "bin/spatial_media")
+except (OSError, IOError):
+    pass
+
 
 setup(name='spatialmedia',
       version='2.1a1',
@@ -6,5 +11,6 @@ setup(name='spatialmedia',
       author='Google Inc',
       license='Apache License 2.0',
       url='https://github.com/google/spatial-media',
-      packages=['spatialmedia', 'spatialmedia.mpeg']
+      packages=['spatialmedia', 'spatialmedia.mpeg'],
+      scripts=['bin/spatial_media']
 )
